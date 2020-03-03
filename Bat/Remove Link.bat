9a2129976a99b2b3eba797f91a78db76499b1d61
Matt <33806410+Matthewzhou809@users.noreply.github.com>
2020-03-03 07:25:35 -0800

    Update Remove Link.bat

--- Bat/Remove Link.bat
+++ Bat/Remove Link.bat
@@ -1,14 +1,14 @@
 @echo off
 REM Removes Symbolic Link
-rmdir "C:\ProgramData\Logitech"
-rmdir "C:\Program Files\Common Files\Logitech" 
-rmdir "C:\Program Files (x86)\Common Files\Logitech" 
-rmdir "C:\Program Files (x86)\Logitech" 
-rmdir "C:\Program Files\Logitech" 
+rmdir "C:\ProgramData\Thunder Network"
+rmdir "C:\Program Files\Common Files\Thunder Network" 
+rmdir "C:\Program Files (x86)\Common Files\Thunder Network" 
+rmdir "C:\Program Files (x86)\Thunder Network" 
+rmdir "C:\Program Files\Thunder Network" 
 
-rmdir "C:\Users\matth\AppData\Local\Logitech"
-rmdir "C:\Users\matth\AppData\Local\Programs\Logitech"
-rmdir "C:\Users\matth\AppData\Roaming\Logitech"
-rmdir "C:\Users\matth\AppData\LocalLow\Logitech" 
+rmdir "C:\Users\matth\AppData\Local\Thunder Network"
+rmdir "C:\Users\matth\AppData\Local\Programs\ Thunder Network"
+rmdir "C:\Users\matth\AppData\Roaming\Thunder Network"
+rmdir "C:\Users\matth\AppData\LocalLow\Thunder Network" 
 
 pause
