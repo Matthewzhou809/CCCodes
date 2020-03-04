d30e3b8728e7f22eabda62b19ab4f2d6ad849ec5
Matt <33806410+Matthewzhou809@users.noreply.github.com>
2020-03-03 20:12:37 -0800

    Update Mklink.bat

--- Bat/Mklink.bat
+++ Bat/Mklink.bat
@@ -1,27 +1,27 @@
 @echo off
 
 REM Create Symbolic Link
-    mklink /d "C:\ProgramData\Thunder Network" "B:\ProgramData\Thunder Network" 
-    mklink /d "C:\Program Files\Common Files\Thunder Network" "B:\Program Files\Common Files\Thunder Network" 
-    mklink /d "C:\Program Files (x86)\Common Files\Thunder Network" "B:\Program Files (x86)\Common Files\Thunder Network" 
-    mklink /d "C:\Program Files (x86)\Thunder Network" "B:\Program Files (x86)\Thunder Network"
-    mklink /d "C:\Program Files\Thunder Network" "B:\Program Files\Thunder Network"
+    mklink /d "C:\ProgramData\Cygwin64" "B:\ProgramData\Cygwin64" 
+    mklink /d "C:\Program Files\Common Files\Cygwin64" "B:\Program Files\Common Files\Cygwin64" 
+    mklink /d "C:\Program Files (x86)\Common Files\Cygwin64" "B:\Program Files (x86)\Common Files\Cygwin64" 
+    mklink /d "C:\Program Files (x86)\Cygwin64" "B:\Program Files (x86)\Cygwin64"
+    mklink /d "C:\Program Files\Cygwin64" "B:\Program Files\Cygwin64"
     
-    mklink /d "C:\Users\matth\AppData\Local\Thunder Network" "B:\Users\AppData\Local\Thunder Network"
-    mklink /d "C:\Users\matth\AppData\Local\Programs\Thunder Network" "B:\Users\AppData\Local\Programs\Thunder Network"
-    mklink /d "C:\Users\matth\AppData\Roaming\Thunder Network" "B:\Users\AppData\Roaming\Thunder Network"
-    mklink /d "C:\Users\matth\AppData\LocalLow\Thunder Network" "B:\Users\AppData\LocalLow\Thunder Network" 
+    mklink /d "C:\Users\matth\AppData\Local\Cygwin64" "B:\Users\AppData\Local\Cygwin64"
+    mklink /d "C:\Users\matth\AppData\Local\Programs\Cygwin64" "B:\Users\AppData\Local\Programs\Cygwin64"
+    mklink /d "C:\Users\matth\AppData\Roaming\Cygwin64" "B:\Users\AppData\Roaming\Cygwin64"
+    mklink /d "C:\Users\matth\AppData\LocalLow\Cygwin64" "B:\Users\AppData\LocalLow\Cygwin64" 
 
 REM Directory Folder
-    mkdir "B:\ProgramData\  Thunder Network" 
-    mkdir "B:\Program Files\Common Files\Thunder Network" 
-    mkdir "B:\Program Files (x86)\Common Files\Thunder Network" 
-    mkdir "B:\Program Files (x86)\Thunder Network"
-    mkdir "B:\Program Files\Thunder Network"
+    mkdir "B:\ProgramData\  Cygwin64" 
+    mkdir "B:\Program Files\Common Files\Cygwin64" 
+    mkdir "B:\Program Files (x86)\Common Files\Cygwin64" 
+    mkdir "B:\Program Files (x86)\Cygwin64"
+    mkdir "B:\Program Files\Cygwin64"
 
-    mkdir "B:\Users\AppData\Local\Thunder Network"
-    mkdir "B:\Users\AppData\Local\Programs\Thunder Network"
-    mkdir "B:\Users\AppData\Roaming\Thunder Network"
-    mkdir "B:\Users\AppData\LocalLow\Thunder Network" 
+    mkdir "B:\Users\AppData\Local\Cygwin64"
+    mkdir "B:\Users\AppData\Local\Programs\Cygwin64"
+    mkdir "B:\Users\AppData\Roaming\Cygwin64"
+    mkdir "B:\Users\AppData\LocalLow\Cygwin64" 
 
 pause
