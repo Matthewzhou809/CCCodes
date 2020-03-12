31bbeb70fe574fd11308ccf0d94833675db631a7
Matt <33806410+Matthewzhou809@users.noreply.github.com>
2020-03-12 13:59:01 -0700

    Update Remove Link.bat

--- Bat/Remove Link.bat
+++ Bat/Remove Link.bat
@@ -1,14 +1,14 @@
 @echo off
 REM Removes Symbolic Link
-rmdir "C:\ProgramData\Thunder Network"
-rmdir "C:\Program Files\Common Files\Thunder Network" 
-rmdir "C:\Program Files (x86)\Common Files\Thunder Network" 
-rmdir "C:\Program Files (x86)\Thunder Network" 
-rmdir "C:\Program Files\Thunder Network" 
+rmdir "C:\ProgramData\Microsoft Office"
+rmdir "C:\Program Files\Common Files\Microsoft Office" 
+rmdir "C:\Program Files (x86)\Common Files\Microsoft Office" 
+rmdir "C:\Program Files (x86)\Microsoft Office" 
+rmdir "C:\Program Files\Microsoft Office" 
 
-rmdir "C:\Users\matth\AppData\Local\Thunder Network"
-rmdir "C:\Users\matth\AppData\Local\Programs\ Thunder Network"
-rmdir "C:\Users\matth\AppData\Roaming\Thunder Network"
-rmdir "C:\Users\matth\AppData\LocalLow\Thunder Network" 
+rmdir "C:\Users\matth\AppData\Local\Microsoft Office"
+rmdir "C:\Users\matth\AppData\Local\Programs\ Microsoft Office"
+rmdir "C:\Users\matth\AppData\Roaming\Microsoft Office"
+rmdir "C:\Users\matth\AppData\LocalLow\Microsoft Office" 
 
 pause
