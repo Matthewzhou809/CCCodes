5072547b9c89b21946da1e61d527d41ef7b76557
Matt <33806410+Matthewzhou809@users.noreply.github.com>
2020-05-06 15:24:04 -0700

    POI

--- C++/Playground.cpp
+++ C++/Playground.cpp
@@ -1,42 +1,51 @@
 #include <iostream>
 #include <cmath>
 
-const int count = 50;
-
-int summ(int t)
+int rt(int long long inn, int long long arr[], int size)
 {
-    
+    int temp = 0, result = 0;
+    for (size_t i = 0; i < size; i++)
+    {
+        temp += arr[i];
+        if (inn <= temp)
+        {
+            result = i;
+            goto solution;
+        }
+        else if (i == size - 1)
+            std::cout << "Out of bound" << std::endl;
+    }
+solution:
+    std::cout << "The depth " << result << " distance away from the root";
+    return result;
 }
 
 int main()
 {
-    bool again = true;
-    int long long bt[count], input, temp, result;
     //initialization
+    struct nodest
+    {
+        int x;
+        int y;
+        int parent;
+    };
+    nodest node[50];
+    const int count = 50;
+    bool again = true;
+    int long long bt[count], input;
     for (size_t i = 0; i < count; i++)
     {
         bt[i] = pow(2, i);
         std::cout << bt[i] << std::endl;
     }
     //loop
-    while (true == again)
+    while (again == true)
     {
-        temp = 0;
+        std::cout << "\nInput a value:\n";
         std::cin >> input;
-        for (size_t i = 0; i < count; i++)
-        {
-            temp+= bt[i];
-            if (input <= temp)
-            {
-                result = i;
-                goto solution;
-            }else if(i == count - 1)
-                std::cout << "Out of bound" << std::endl;
-        }
-        solution:
-            std::cout << "The depth " << result << " distance away from the root";
+        rt(input, bt, count);
+        std::cout << "\nAgain?:\n";
+        std::cin >> again;
     }
-
-
     return 0;
 }
