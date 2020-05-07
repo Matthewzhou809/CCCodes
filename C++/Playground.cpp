bd4550ee346d53aff591265effbb7d7be8a91003
Matt <33806410+Matthewzhou809@users.noreply.github.com>
2020-05-07 04:07:35 -0700

    Gaveyo

--- C++/Playground.cpp
+++ C++/Playground.cpp
@@ -1,67 +1,26 @@
 #include <iostream>
 #include <cmath>
+// Calvulate Pie
 
-int rt(int long long inn, int long long arr[], int size)
-{
-    int temp = 0, result = 0;
-    for (size_t i = 0; i < size; i++)
-    {
-        temp += arr[i];
-        if (inn <= temp)
-        {
-            result = i;
-            goto solution;
-        }
-        else if (i == size - 1)
-            std::cout << "Out of bound" << std::endl;
-    }
-solution:
-    std::cout << "\nThe depth " << result << " distance away from the root\n";
-    return result;
-}
+const int cap = 8;
+double temp, curp, arr[cap][cap], result;
 
-int rtsum(int in)
-{
-    int result = 0;
-    double current = in;
-    while (current != 1)
-    {
-        result += current;
-        current = current / 2;
-        current = floor(current);
-        std::cout << "\nThe current value is " << current << "\n";
-    }
-    std::cout << "\nThe sum is " << result << "\n";
-    return result;
-}
 int main()
 {
-    const int count = 50, CAP = 100000;
-    //initialization
-    // struct nodest
-    // {
-    //     int x;
-    // int lvl;
-    //     int parent;
-    // };
-    // nodest node[CAP];
-    bool again = true;
-    int long long bt[count], input;
-    for (size_t i = 0; i < count; i++)
+    //initialize points
+    for (size_t i = 0; i < cap; i++)
     {
-        bt[i] = pow(2, i);
-        std::cout << bt[i] << std::endl;
+        arr[i][i] = ;
     }
+    
 
-    //loop
-    while (again == true)
+    for (size_t i = 0; i < cap; i++)
     {
-        std::cout << "\nInput a value:\n";
-        std::cin >> input;
-        rt(input, bt, count);
-        rtsum(input);
-        std::cout << "\nAgain?:\n";
-        std::cin >> again;
+        curp = 
+        temp = sqrt(pow(cos(curp), 2) + pow(sin(curp), 2));
+        result += temp;
     }
+    
+    
     return 0;
 }
