feac303dc6a360bd6342b20b5c4451d25fbb59dd
Matt <33806410+Matthewzhou809@users.noreply.github.com>
2020-05-24 03:27:54 -0700

    Update Mklink.bat

--- Bat/Mklink.bat
+++ Bat/Mklink.bat
@@ -1,27 +1,27 @@
 @echo off
 
 REM Create Symbolic Link
-    mklink /d "C:\ProgramData\GitHubDesktop" "B:\ProgramData\GitHubDesktop" 
-    mklink /d "C:\Program Files\Common Files\GitHubDesktop" "B:\Program Files\Common Files\GitHubDesktop" 
-    mklink /d "C:\Program Files (x86)\Common Files\GitHubDesktop" "B:\Program Files (x86)\Common Files\GitHubDesktop" 
-    mklink /d "C:\Program Files (x86)\GitHubDesktop" "B:\Program Files (x86)\GitHubDesktop"
-    mklink /d "C:\Program Files\GitHubDesktop" "B:\Program Files\GitHubDesktop"
+    mklink /d "C:\ProgramData\Bridge" "B:\ProgramData\Bridge" 
+    mklink /d "C:\Program Files\Common Files\Bridge" "B:\Program Files\Common Files\Bridge" 
+    mklink /d "C:\Program Files (x86)\Common Files\Bridge" "B:\Program Files (x86)\Common Files\Bridge" 
+    mklink /d "C:\Program Files (x86)\Bridge" "B:\Program Files (x86)\Bridge"
+    mklink /d "C:\Program Files\Bridge" "B:\Program Files\Bridge"
     
-    mklink /d "C:\Users\matth\AppData\Local\GitHubDesktop" "B:\Users\AppData\Local\GitHubDesktop"
-    mklink /d "C:\Users\matth\AppData\Local\Programs\GitHubDesktop" "B:\Users\AppData\Local\Programs\GitHubDesktop"
-    mklink /d "C:\Users\matth\AppData\Roaming\GitHubDesktop" "B:\Users\AppData\Roaming\GitHubDesktop"
-    mklink /d "C:\Users\matth\AppData\LocalLow\GitHubDesktop" "B:\Users\AppData\LocalLow\GitHubDesktop" 
+    mklink /d "C:\Users\matth\AppData\Local\Bridge" "B:\Users\AppData\Local\Bridge"
+    mklink /d "C:\Users\matth\AppData\Local\Programs\Bridge" "B:\Users\AppData\Local\Programs\Bridge"
+    mklink /d "C:\Users\matth\AppData\Roaming\Bridge" "B:\Users\AppData\Roaming\Bridge"
+    mklink /d "C:\Users\matth\AppData\LocalLow\Bridge" "B:\Users\AppData\LocalLow\Bridge" 
 
 REM Directory Folder
-    mkdir "B:\ProgramData\GitHubDesktop" 
-    mkdir "B:\Program Files\Common Files\GitHubDesktop" 
-    mkdir "B:\Program Files (x86)\Common Files\GitHubDesktop" 
-    mkdir "B:\Program Files (x86)\GitHubDesktop"
-    mkdir "B:\Program Files\GitHubDesktop"
+    mkdir "B:\ProgramData\Bridge" 
+    mkdir "B:\Program Files\Common Files\Bridge" 
+    mkdir "B:\Program Files (x86)\Common Files\Bridge" 
+    mkdir "B:\Program Files (x86)\Bridge"
+    mkdir "B:\Program Files\Bridge"
 
-    mkdir "B:\Users\AppData\Local\GitHubDesktop"
-    mkdir "B:\Users\AppData\Local\Programs\GitHubDesktop"
-    mkdir "B:\Users\AppData\Roaming\GitHubDesktop"
-    mkdir "B:\Users\AppData\LocalLow\GitHubDesktop" 
+    mkdir "B:\Users\AppData\Local\Bridge"
+    mkdir "B:\Users\AppData\Local\Programs\Bridge"
+    mkdir "B:\Users\AppData\Roaming\Bridge"
+    mkdir "B:\Users\AppData\LocalLow\Bridge" 
 
 pause
