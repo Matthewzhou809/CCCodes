379623dacae7780ca0a4aad5c7661f37ecef29f1
Matt <33806410+Matthewzhou809@users.noreply.github.com>
2020-05-24 02:22:37 -0700

    Update source.py

--- Python/source.py
+++ Python/source.py
@@ -1,3 +1,6 @@
-import os
-print(os.popen('xsel').read())
+import pytube
 
+from pytube import YouTube
+yt = pytube.YouTube('https://www.youtube.com/watch?v=xtZOnyYu16U')
+ft = yt.get('mp4', '720p')
+ft.download('/path/to/download/directory')
