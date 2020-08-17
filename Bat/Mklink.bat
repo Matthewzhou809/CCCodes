cd486c50804cef1b85982260b11080c649b3542c
Matt <33806410+Matthewzhou809@users.noreply.github.com>
2020-08-17 04:38:09 -0700

    Update Mklink.bat

--- Bat/Mklink.bat
+++ Bat/Mklink.bat
@@ -1,27 +1,27 @@
 @echo off
 
 REM Create Symbolic Link
-    mklink /d "C:\ProgramData\Forge" "B:\ProgramData\Forge" 
-    mklink /d "C:\Program Files\Common Files\Forge" "B:\Program Files\Common Files\Forge" 
-    mklink /d "C:\Program Files (x86)\Common Files\Forge" "B:\Program Files (x86)\Common Files\Forge" 
-    mklink /d "C:\Program Files (x86)\Forge" "B:\Program Files (x86)\Forge"
-    mklink /d "C:\Program Files\Forge" "B:\Program Files\Forge"
+    mklink /d "C:\ProgramData\Discord" "B:\ProgramData\Discord" 
+    mklink /d "C:\Program Files\Common Files\Discord" "B:\Program Files\Common Files\Discord" 
+    mklink /d "C:\Program Files (x86)\Common Files\Discord" "B:\Program Files (x86)\Common Files\Discord" 
+    mklink /d "C:\Program Files (x86)\Discord" "B:\Program Files (x86)\Discord"
+    mklink /d "C:\Program Files\Discord" "B:\Program Files\Discord"
     
-    mklink /d "C:\Users\matth\AppData\Local\Forge" "B:\Users\AppData\Local\Forge"
-    mklink /d "C:\Users\matth\AppData\Local\Programs\Forge" "B:\Users\AppData\Local\Programs\Forge"
-    mklink /d "C:\Users\matth\AppData\Roaming\Forge" "B:\Users\AppData\Roaming\Forge"
-    mklink /d "C:\Users\matth\AppData\LocalLow\Forge" "B:\Users\AppData\LocalLow\Forge" 
+    mklink /d "C:\Users\matth\AppData\Local\Discord" "B:\Users\AppData\Local\Discord"
+    mklink /d "C:\Users\matth\AppData\Local\Programs\Discord" "B:\Users\AppData\Local\Programs\Discord"
+    mklink /d "C:\Users\matth\AppData\Roaming\Discord" "B:\Users\AppData\Roaming\Discord"
+    mklink /d "C:\Users\matth\AppData\LocalLow\Discord" "B:\Users\AppData\LocalLow\Discord" 
 
 REM Directory Folder
-    mkdir "B:\ProgramData\Forge" 
-    mkdir "B:\Program Files\Common Files\Forge" 
-    mkdir "B:\Program Files (x86)\Common Files\Forge" 
-    mkdir "B:\Program Files (x86)\Forge"
-    mkdir "B:\Program Files\Forge"
+    mkdir "B:\ProgramData\Discord" 
+    mkdir "B:\Program Files\Common Files\Discord" 
+    mkdir "B:\Program Files (x86)\Common Files\Discord" 
+    mkdir "B:\Program Files (x86)\Discord"
+    mkdir "B:\Program Files\Discord"
 
-    mkdir "B:\Users\AppData\Local\Forge"
-    mkdir "B:\Users\AppData\Local\Programs\Forge"
-    mkdir "B:\Users\AppData\Roaming\Forge"
-    mkdir "B:\Users\AppData\LocalLow\Forge" 
+    mkdir "B:\Users\AppData\Local\Discord"
+    mkdir "B:\Users\AppData\Local\Programs\Discord"
+    mkdir "B:\Users\AppData\Roaming\Discord"
+    mkdir "B:\Users\AppData\LocalLow\Discord" 
 
 pause
